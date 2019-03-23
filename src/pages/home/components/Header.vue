21bdabf43b710585b05e4db5f6b1844334d9fcd0
故事很长 <1316727734@qq.com>
2019-03-23 21:59:16 +0800

    init city-router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
            <span class="iconfont">&#xe632;</span>
            输入城市/景点/游玩主题
         </div>
+       <router-link to="/city">
        <div class="header-right">
            {{ this.city }}
            <sapn class="iconfont arrow-icon">&#xe64a;</sapn>
        </div>
+       </router-link>
     </div>
 </template>
 <script>
@@ -51,6 +53,7 @@ export default {
        .header-right{
            width: 1.24rem;
            float: right;
+           color:#fff;
            text-align: center;
            .arrow-icon{
                margin-left: .1rem;
