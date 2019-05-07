951866818d4e854239b3680100fba7b98efce030
Unknown <1316727734@qq.com>
2019-05-07 17:31:24 +0800

    格式修改

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -18,8 +18,10 @@ export default {
        return{
             swiperOption: {
                  pagination: '.swiper-pagination',
+                 autoplay:2000,
                  loop:true
-            }
+            },
+            swiperSlides: [1, 2, 3, 4]
        }
     },
     computed: {
